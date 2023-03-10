5bbb3785bf39714e966cb758615d5f20b4d26375
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-03-10 17:40:03 +0100

    bellakeras JS tambien

---
+++ CursoWeb/JS/miCodigoJavaScript.js
@@ -0,0 +1,12 @@
+function cambiaParrafo() {
+
+    document.getElementById("cambia").innerHTML =
+        "Párrafo cambiado";
+
+}
+function cambiah2() {
+
+    document.getElementById("cambia2").innerHTML =
+        "Párrafo cambiado";
+
+}
