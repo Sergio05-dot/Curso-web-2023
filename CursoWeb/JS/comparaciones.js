21d1e349197b64c549427ed765bbc9f837a95c1e
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-03-14 17:49:24 +0100

    operadores de js

---
+++ CursoWeb/JS/comparaciones.js
@@ -0,0 +1,7 @@
+var x, y;
+x=7;
+y=4;
+document.getElementById("result1").innerHTML = x==y
+document.getElementById("result2").innerHTML = x===y
+document.getElementById("result3").innerHTML = x!=y
+document.getElementById("result4").innerHTML = x!==y 
