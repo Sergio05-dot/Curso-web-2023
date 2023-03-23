f2c82ae8e795b8a42a3d4e5272239903a7ff82b1
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-03-23 18:17:29 +0100

    eventos de js

---
+++ CursoWeb/JS/eventosjavascript.js
@@ -0,0 +1,13 @@
+// Eventos de Javascript
+// onclick="document.getElementById('demo').innerHTML=Date()">"The time is?"
+
+// function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+//   }
+//   function displayDate() {
+//     document.getElementById("demo1").innerHTML = Date();
+//   }
+
+function rueda() {
+    document.getElementById("ruedaraton").innerHTML  ;
+} 
